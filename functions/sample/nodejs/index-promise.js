6623450a8033e06c90b50f0f6f9239d1b388c9a5
Haku <srsparks15@gmail.com>
2022-12-01 02:57:29 -0500

    Modified some files

--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -6,22 +6,19 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
-
-    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-    const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
+    cloudant({
+        url: params.COUCH_URL,
+        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
     });
-    cloudant.setServiceUrl(params.COUCH_URL);
-
     let dbListPromise = getDbs(cloudant);
     return dbListPromise;
 }
 
 function getDbs(cloudant) {
      return new Promise((resolve, reject) => {
-         cloudant.getAllDbs()
+         cloudant.db.list()
              .then(body => {
-                 resolve({ dbs: body.result });
+                 resolve({ dbs: body });
              })
              .catch(err => {
                   console.log(err);
